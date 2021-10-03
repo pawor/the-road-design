76f3534fae6eb3208705232262cf22e1cbbefab8
pawor <pawor.emi@gmail.com>
2021-10-03 12:57:53 +0700

    add card

---
+++ script.js
@@ -0,0 +1,10 @@
+Array.from(document.querySelectorAll(
+    ".navigation-button")).forEach((item) => {
+        item.onclick = () => {
+            item.parentElement.parentElement.classList.toggle(
+                "change"
+            );
+        };
+    }  
+);
+
